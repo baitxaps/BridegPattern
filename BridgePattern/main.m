2dfe26ca394ac5b929d24b17ce616d5ee329df82
rhc <bawfnhaps@163.com>
2016-08-12 19:12:44 +0800

    Initial Commit

---
+++ BridgePattern/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BridgePattern
+//
+//  Created by hairong chen on 16/8/12.
+//  Copyright © 2016年 hairong chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
