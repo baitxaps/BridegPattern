df5bf87863d9a1d9457ae12b3064cc987ad6c101
rhc <bawfnhaps@163.com>
2016-08-12 21:26:46 +0800

    BridgePattern

--- BridgePattern/main.m
+++ BridgePattern/main.m
@@ -7,11 +7,31 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "HouseCorp.h"
+#import "ClothesCorp.h"
+#import "House.h"
+#import "IPod.h"
+#import "ConcreteImplementor1.h"
+#import "RefinedAbstraction.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        // 1
+        House *house = [House new];
+        Corp *houseCorp = [[HouseCorp alloc]initWithProduce:house];
+        [houseCorp makeMoney];
+        
+        Corp *clothesCorp = [[ClothesCorp alloc]initWithProduce:[IPod new]];
+        [clothesCorp makeMoney];
+        
+        // 2
+        // 定义一个实现化角色
+        id<Implementor>imp = [ConcreteImplementor1 new];
+        // 定义一个抽象化角色
+        Abstraction *abs = [[RefinedAbstraction alloc]initWithImplementor:imp];
+        // 执行行文
+        [abs request];
+        
     }
     return 0;
 }
